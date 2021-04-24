65f4c1b8fc5a220c10151e57925a7ef0ac643606
Jason Hammock <jason@arrayschool.com>
2021-04-24 15:43:28 -0600

    reduce number of pieces

--- scripts/populateDb.mjs
+++ scripts/populateDb.mjs
@@ -2,7 +2,7 @@ import { generateFakes } from '../utils/genData.mjs'
 import { writeToDb } from '../utils/writeToDb.mjs'
 
 const data = {
-  artPieces: generateFakes('art_piece', 15),
+  artPieces: generateFakes('art_piece', 10),
 }
 
 writeToDb(data)
